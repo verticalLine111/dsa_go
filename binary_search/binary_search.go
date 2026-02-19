d850b9b95594fef044a20075a24b1f0153dffd67
WooJongSeon <jswoo030@gmail.com>
2026-02-20 02:21:18 +0900

    binary search

--- binary_search/binary_search.go
+++ binary_search/binary_search.go
@@ -1,22 +1,28 @@
-package binarysearch
+package main
 
-func search()  {
-	
+
+func search(arr []int, target int) int {
+	left := 0
+	right := len(arr) - 1
+	for left <= right {
+		mid := (left + right) / 2
+		if arr[mid] == target {
+			return mid
+		} else {
+			if arr[mid] < target {
+				left = mid + 1
+			} else {
+				right = mid -1
+			}
+		}
+	}
+	return -1
 }
 
-func searchInsertPosition()  {
-	
-}
-
-func lowerBound()  {
-	
-}
-func upperBound(arr []int, val int)  {
-	
-}
 
 func main()  {
-	arr := []int { 1,1,2,3,4,4,4};
-	upperBound(arr, 4)
+	arr := []int {1,2,3,4,5,6,7,8,9,10};
+	idx := search(arr, 4)
+	println(idx)
 	
 }

546d134f145462b5fc6fcf09bd3861755bcd0a83
WooJongSeon <jswoo030@gmail.com>
2026-02-19 20:22:02 +0900

    create folders

---
+++ binary_search/binary_search.go
@@ -0,0 +1,22 @@
+package binarysearch
+
+func search()  {
+	
+}
+
+func searchInsertPosition()  {
+	
+}
+
+func lowerBound()  {
+	
+}
+func upperBound(arr []int, val int)  {
+	
+}
+
+func main()  {
+	arr := []int { 1,1,2,3,4,4,4};
+	upperBound(arr, 4)
+	
+}
---
+++ linked_list/linked_list.go
@@ -0,0 +1,31 @@
+package linked_list
+
+func pushFront()  {
+	
+}
+
+func pushBack()  {
+	
+}
+func popFront()  {
+	
+}
+func popBack()  {
+	
+}
+func insertAt()  {
+	
+}
+func removeAt()  {
+	
+}
+
+func get(index int)  {
+	
+}
+func reverse()  {
+	
+}
+func size()  {
+	
+}
